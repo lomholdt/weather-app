d9e58a71c5ad63923c0c6185b605b79edcf269a6
jonastonny <jonastonnynielsen@gmail.com>
2015-07-14 21:59:23 +0200

    Added country to weather location

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	<div>
 		@foreach ($owm_output->list as $owm)
-	    <h4>{{ $owm->name }}</h4>
+	    <h4>{{ $owm->name }}, {{ $owm->sys->country }}</h4>
 	    <p>{{ $owm->main->temp }} (OpenWeatherMap.org)</p>
 		@endforeach
 	</div>
