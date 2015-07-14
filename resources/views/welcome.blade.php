ed40653b985403f18cea2c9d5066a6b64a9c321b
jonaslomholdt <jonaslomholdt@gmail.com>
2015-07-14 18:45:40 +0200

    Added welcome message!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <div>Weather App Project</div>
             </div>
         </div>
     </body>
