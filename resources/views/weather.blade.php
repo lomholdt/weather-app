16d9f354032fe9eed0e78b44e905072f034bbbb7
jonastonny <jonastonnynielsen@gmail.com>
2015-07-15 16:30:24 +0200

    Added support for showing description if available and added to weather object

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -10,6 +10,9 @@
             <div class="card-content white-text">
               <span class="card-title">{{ $w->getOrigin() }}</span>
               <p>{{ $w->getTemp() }} &deg;C</p>
+              @if($w->getDescription() != null)
+              	<p>{{ $w->getDescription() }}</p>
+              @endif
               <p>{{ $w->getCountry() }}</p>
               <p>{{ $w->getCity() }}</p>
             </div>
