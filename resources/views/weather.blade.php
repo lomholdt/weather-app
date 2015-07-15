72abce425e602a1930dfe40f730c0a8a9f624f57
jonastonny <jonastonnynielsen@gmail.com>
2015-07-15 17:02:52 +0200

    Added description from Yahoo Weather

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -8,13 +8,15 @@
         <div class="col s12 m6">
           <div class="card blue-grey darken-1">
             <div class="card-content white-text">
-              <span class="card-title">{{ $w->getOrigin() }}</span>
-              <p>{{ $w->getTemp() }} &deg;C</p>
-              @if($w->getDescription() != null)
-              	<p>{{ $w->getDescription() }}</p>
-              @endif
-              <p>{{ $w->getCountry() }}</p>
-              <p>{{ $w->getCity() }}</p>
+            	<span class="card-title">{{ $w->getOrigin() }}</span>
+             	<p>{{ $w->getTemp() }} &deg;C</p>
+             	@if($w->getDescription())
+              		<p>{{ $w->getDescription() }}</p>
+              	@else
+              		<p>&nbsp;</p>
+             	@endif
+             	<p>{{ $w->getCountry() }}</p>
+             	<p>{{ $w->getCity() }}</p>
             </div>
           </div>
         </div>
